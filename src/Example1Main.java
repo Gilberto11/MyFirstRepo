43dfa618b182c4458e2a0eeae93b9934b2a1fb43
Gilberto11 <surfcorinthians@hotmail.com>
2016-04-27 16:10:48 +0100

    first program

---
+++ src/Example1Main.java
@@ -0,0 +1,30 @@
+
+public class Example1Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int wholenumber = 10;
+		float decimalNumber = 3.4F;
+		double bidDecimalNumber = 1304.3467;
+		char myLetter = 'C';
+		boolean found = true;
+		short smallwholenumber = 10;
+		//declare variable
+		int x,y,z;
+		double average;
+		
+		//initialising the variable
+		x = 10;
+		y = 12;
+		//processing - adding the values together
+		
+		z = x + y;
+		average = (x + y) / 2;
+		//output the result
+		System.out.println (" The sum is: " + z);
+		System.out.println("The average is: " + average);
+		
+
+	}
+
+}
